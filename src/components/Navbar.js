bcf5e3c780c99bc541519c4b7a8e885d4085d369
Darsh Mashru <mashrudarsh31@gmail.com>
2023-11-03 13:57:53 +0530

    Exec Recruitment NavBar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,83 @@
-import React from 'react'
-import tqlogo from "../assets/tqlogo3.png"
+import React from "react";
+import tqlogo from "../assets/tqlogo3.png";
 import "aos/dist/aos.css";
 
-
-export default function Navbar()
-{
-
-    return (
-        <>
-        <div>
-            <nav className="navbar shadow-5-strong p-navbar fixed-top">
-                <div className="menu-wrap">
-                    <input type="checkbox" className="toggler" />
-                    <div className="hamburger"><div></div></div>
-                    <div className="menu">
-                        <div>
-                            <div>
-                                <ul>
-                                    <li><a className='fw-bold' href="/">Home</a></li>
-                                    <li><a className='fw-bold' href="abouttq">About TQ</a></li>
-                                    <li><a className='fw-bold' href="schedule">Schedule</a></li>
-                                    <li><a className='fw-bold' href="workshops">Workshops</a></li>
-                                    <li><a className='fw-bold' href="events">Events</a></li>
-                                    <li><a className='fw-bold' href="collab">Collaborations</a></li>
-                                    <li><a className='fw-bold' href="sponsors">Sponsors</a></li>
-                                    <li><a className='fw-bold' href="rewind">Rewind</a></li>
-                                    <li><a className='fw-bold' href="contactus">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+export default function Navbar() {
+  return (
+    <>
+      <div>
+        <nav className="navbar shadow-5-strong p-navbar fixed-top">
+          <div
+            className="menu-wrap"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <div className="hamburger" style={{ marginRight: "10px" }}>
+              <div></div>
+            </div>
+            <div className="menu">
+              <div>
+                <div>
+                  <ul>
+                    <li>
+                      <a className="fw-bold" href="/">
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="abouttq">
+                        About TQ
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="schedule">
+                        Schedule
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="workshops">
+                        Workshops
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="events">
+                        Events
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="collab">
+                        Collaborations
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="sponsors">
+                        Sponsors
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="rewind">
+                        Rewind
+                      </a>
+                    </li>
+                    <li>
+                      <a className="fw-bold" href="contactus">
+                        Contact
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-                <a className="navbar-brand fw-bold" href="contactus"></a>
-                <a className="navbar-brand" href="/">
-                    <img src={tqlogo} width="50" height="50" alt="" />
-                </a>
-            </nav>
-        </div>
-        
-
-        </>
-    )
+              </div>
+            </div>
+          </div>
+          <a className="navbar-brand fw-bold" href="contactus"></a>
+          <a
+            className="navbar-brand"
+            href="/"
+            style={{ marginTop: "-25    px" }}
+          >
+            <img src={tqlogo} width="50" height="50" alt="" />
+          </a>
+        </nav>
+      </div>
+    </>
+  );
 }
--- src/pages/ExecRecruitment.js
+++ src/pages/ExecRecruitment.js
@@ -1,3 +1,27 @@
+// import React, { useEffect } from 'react';
+
+// function ExecRecruitment() {
+//     useEffect(() => {
+//         const script = document.createElement('script');
+//         script.src = "https://tally.so/widgets/embed.js";
+//         script.onload = () => {
+//             if(window.Tally) {
+//                 window.Tally.loadEmbeds();
+//             }
+//         }
+//         document.body.appendChild(script);
+//     }, []);
+
+//     return (
+//         <>
+//         {/* <script src="https://tally.so/widgets/embed.js"></script> */}
+//         <iframe data-tally-src="https://tally.so/embed/w8LlpY?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="1146" frameborder="0" marginheight="0" marginwidth="0" title="Executive Registrations"></iframe>
+//         </>
+//     )
+// }
+
+// export default ExecRecruitment;
+
 import React, { useEffect } from 'react';
 
 function ExecRecruitment() {
@@ -13,10 +37,10 @@ function ExecRecruitment() {
     }, []);
 
     return (
-        <>
-        {/* <script src="https://tally.so/widgets/embed.js"></script> */}
-        <iframe data-tally-src="https://tally.so/embed/w8LlpY?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="1146" frameborder="0" marginheight="0" marginwidth="0" title="Executive Registrations"></iframe>
-        </>
+        <div style={{ paddingTop: '100px' }}> {/* Adjust this value based on the height of your navbar */}
+            {/* <script src="https://tally.so/widgets/embed.js"></script> */}
+            <iframe data-tally-src="https://tally.so/embed/w8LlpY?alignLeft=1&dynamicHeight=1" loading="lazy" width="100%" height="1146" frameborder="0" marginheight="0" marginwidth="0" title="Executive Registrations"></iframe>
+        </div>
     )
 }
 
